d7773edb610a19345654b9581ece4f979c2a8072
Casey Ochieng <caseyochieng46@gmail.com>
2022-12-06 14:34:17 +0300

    completed

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-function addingEventListener() {
+function addEventListener(){
+    const input =documentById("button")
+
+    function clickAlert(){
+        alert("I was clicked!")
+    }
+    input.addEventListener("click",clickAlert)
 }
